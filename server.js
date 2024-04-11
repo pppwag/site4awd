67edf75498771ebda8c10bda5ac3339a393e03bc
Par1y <xzfiszoombie@163.com>
2024-04-11 19:12:15 +0800

    总之做了很多优化

--- server.js
+++ server.js
@@ -1,21 +1,26 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const path = require('path');
+const ejs = require('ejs');
 const app = express();
-const route = require("./routers/mainRoute");
+const index = require("./routers/index");
 const login = require("./routers/login");
 const edit = require("./routers/edit");
 const upload = require('./routers/upload');
 const register = require('./routers/register');
+
+app.set('view engine', 'ejs');		//渲染引擎
+app.engine('.html', ejs.__express)
 
 app.use(cors());	//开启跨域支持
 app.use(bodyParser.json());		//解析json表单
 app.use(bodyParser.urlencoded({extended: true}));
 
 //使用目录public注册静态资源
-app.use('/public/', express.static('./public/'));
+app.use(express.static('./public/'));
 
-app.use("/", route);	//注册主页&关于页面路由
+app.use("/", index);	//注册主页&关于页面路由
 app.use("/login", login);	//注册登录页面路由
 app.use('/register', register);	//注册页面路由
 app.use('/upload', edit);	//注册上传页面路由
