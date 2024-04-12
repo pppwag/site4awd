b20d61df4d6af31adb41a836732fdf567e512b5d
Par1y <xzfiszoombie@163.com>
2024-04-12 18:54:05 +0800

    加上了缺失的文件

---
+++ routers/index.js
@@ -0,0 +1,16 @@
+const path = require('path');
+const express = require("express");
+const router = express.Router();	//注册路由模块
+
+router.get("/", (req, res) => {		//处理默认目录get请求
+	res.render('index.html');
+	console.log("[info] " + req.ip + " requested: index.html");
+});
+
+router.get("/about", (req, res) => {	//处理about目录get请求
+	res.sendFile('about.html');
+	ip = req.ip
+	console.log("[info] " + req.ip + " requested: about.html");
+});
+
+module.exports = router;		//暴露路由模块
