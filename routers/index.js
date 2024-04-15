c3fc0c343f147c135ab9139bc9b5d650f90e27c1
Par1y <xzfiszoombie@163.com>
2024-04-15 20:52:56 +0800

    fix bug

--- routers/index.js
+++ routers/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();	//注册路由模块
+const databaseController = require('../controller/databaseController');
 
 router.get("/", (req, res) => {		//处理默认目录get请求
 	res.render('index.html');
@@ -11,5 +12,11 @@ router.get("/about", (req, res) => {	//处理about目录get请求
 	ip = req.ip
 	console.log("[info] " + req.ip + " requested: about.html");
 });
+
+router.get("/picture", (req, res) => {
+		id = req.body.id;
+		result = databaseController.search();
+		res.send(path);
+})
 
 module.exports = router;		//暴露路由模块
